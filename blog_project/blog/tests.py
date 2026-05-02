47a5bd77698bcdc46a3ffd9e5e47ea5aef015f77
Ubuntu <ubuntu@ip-172-31-19-132.ec2.internal>
2026-05-02 18:32:25 +0000

    Complete blog app sprint 1

---
+++ blog_project/blog/tests.py
@@ -0,0 +1,26 @@
+from django.test import TestCase
+from .models import Post
+
+class PostModelTest(TestCase):
+
+    def setUp(self):
+        Post.objects.create(title="Test Post", content="This is a test.")
+
+    def test_post_content(self):
+        post = Post.objects.get(id=1)
+        expected_object_name = f'{post.title}'
+        self.assertEqual(expected_object_name, 'Test Post')
+
+
+class PostListViewTest(TestCase):
+
+    def setUp(self):
+        Post.objects.create(title="Another Post", content="More content.")
+
+    def test_view_url_exists_at_proper_location(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_uses_correct_template(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'blog/post_list.html')
--- blog_project/blog_project/settings.py
+++ blog_project/blog_project/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-5eo9j7iwk=6qyvxp(wqqgx0^5@4%@@#j2s8fc@9d#i&a67&8hd
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['52.87.167.39', '127.0.0.1', 'localhost']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog',
 ]
 
 MIDDLEWARE = [
@@ -74,12 +75,14 @@ WSGI_APPLICATION = 'blog_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'blog_db',
+        'USER': 'root',
+        'PASSWORD': 'your_mysql_password',
+        'HOST': '127.0.0.1',
+        'PORT': '3307' if os.getenv('CI') else '3306',
     }
 }
-
-
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
